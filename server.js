94dd5616c77032e5bb45e63c7c408f67f647c644
Karol Kluba <karollo9921@gmail.com>
2025-05-02 12:19:14 +0200

    create two endpoints

---
+++ server.js
@@ -0,0 +1,83 @@
+import express from "express";
+import fs from "fs/promises";
+import bodyParser from "body-parser";
+
+const app = express();
+const PORT = 3000;
+
+app.use(bodyParser.json());
+
+const loadData = async () => {
+  const [personsRaw, collegesRaw, researchesRaw] = await Promise.all([
+    fs.readFile("./data/persons.json", "utf-8"),
+    fs.readFile("./data/colleges.json", "utf-8"),
+    fs.readFile("./data/researches.json", "utf-8"),
+  ]);
+  return {
+    persons: JSON.parse(personsRaw),
+    colleges: JSON.parse(collegesRaw),
+    researches: JSON.parse(researchesRaw),
+  };
+};
+
+app.post("/tool1", async (req, res) => {
+  const input = req.body.input;
+
+  if (input.startsWith("test")) {
+    return res.json({ output: input });
+  }
+
+  const { researches } = await loadData();
+
+  const research = researches.find((b) =>
+    b.name.toLowerCase().includes("podróże w czasie")
+  );
+
+  if (!research) {
+    return res.json({ output: "Nie znaleziono takich badań" })
+  }
+
+  const output = `Uczelnia: ${research?.college}, Sponsor: ${research.sponsor}`;
+
+  return res.json({ output });
+});
+
+app.post("/tool2", async (req, res) => {
+  const input = req.body.input;
+
+  if (input.startsWith("test")) {
+    return res.json({ output: input });
+  }
+
+  const collegeId = input.match(/Uczelnia:\s*(\w+)/)?.[1];
+
+  if (!collegeId) {
+    return res.json({ output: "Zadanie nie powiodło się" });
+  }
+
+  if (input.toLowerCase().includes("podaj ludzi")) {
+    const { persons } = await loadData();
+
+    const foundPeople = persons
+      .filter((o) => o.college === collegeId)
+      .map((o) => `${o.firstName} ${o.lastName}`);
+
+    return res.json({ output: foundPeople.join(", ") })
+  }
+
+  if (input.toLowerCase().includes("podaj nazwę uczelni") || input.toLowerCase().includes("podaj nazwe uczelni")) {
+    const { colleges } = await loadData();
+
+    const foundCollege = colleges.find((c) => c.id === collegeId);
+
+    if (!foundCollege) return res.json({ output: "Nie znaleziono uczelni" });
+
+    return res.json({ output: foundCollege.name })
+  }
+
+  return res.json({ output: "Zadanie nie powiodło się" });
+});
+
+app.listen(PORT, () => {
+  console.log(`API listening on port ${PORT}`);
+});
